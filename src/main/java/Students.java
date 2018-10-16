d097e1750214de1460bee9205be96d907cf9dce7
Erwin Suarez <Erwin.Suarez@students.ittralee.ie>
2018-10-16 11:04:31 +0100

    Initial commit

---
+++ src/main/java/Students.java
@@ -0,0 +1,19 @@
+public class Students {
+
+    private String fname;
+    private String sname;
+    private String course;
+    private int courseYear;
+
+    public void setFname(String fname) { this.fname = fname; }
+    public void setSname(String sname) { this.sname = sname; }
+    public void setCourse(String course) { this.course = course; }
+    public void setCourseYear(int courseYear) { this.courseYear = courseYear; }
+
+    public String getFname() { return fname; }
+    public String getSname() { return sname; }
+    public String getCourse() { return course; }
+    public int getCourseYear() { return courseYear; }
+
+
+}
